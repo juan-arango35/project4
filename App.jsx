fbdcf2fd702bfd232f7f89ccefacce997feb2e2b
juan <juanarangozapata35@gmail.com>
2023-04-26 09:56:40 -0500

    agregando estilos de flexbox

--- App.jsx
+++ App.jsx
@@ -1,10 +1,66 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { StyleSheet } from 'react-native'
+
+
 
 export default function App() {
+
+  
   return (
-    <View>
-      <Text>Hola desde react native</Text>
+    <View style={styles.principal} >
+      <View style={styles.container} >
+      <Text style={styles.contenedortext} >dentro de la primera caja </Text>
     </View>
+
+    <View style={styles.container2}>
+      <Text style={styles.contendortexto2}>dentro de la segunda caja</Text>
+    </View>
+
+    </View>
+
   )
+  
 }
+
+const styles=StyleSheet.create({
+
+  principal: {
+
+    flex: 1, 
+    width: "100%",
+
+    backgroundColor: "green"
+  },
+
+  container: {
+
+    flex: 1,
+    backgroundColor: "red",
+    height: 45,
+    width: "100%"
+    
+  },
+
+  contenedor : {
+    color: "red"
+  },
+
+  container2: {
+    flex: 1,
+
+ 
+     backgroundColor: "blue"
+    
+   
+  },
+
+  contenedortext: {
+    color: "white"
+  },
+  contendortexto2: {
+    color: "gray"
+  }
+})
+
+
