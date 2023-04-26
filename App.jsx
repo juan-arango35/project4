2f20e27df7244ae5a2ee8504be58d960895ef14f
juan <juanarangozapata35@gmail.com>
2023-04-26 09:15:28 -0500

    mi primer commit de iniciar el proyecto

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hola desde react native</Text>
+    </View>
+  )
+}
